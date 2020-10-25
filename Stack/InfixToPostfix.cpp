22180d439f6b0027fa0ab27a773eb7c81e71f3b4
Royal-lobster <srujangs8@gmail.com>
2020-10-25 12:34:15 +0530

    finished stack

--- Stack/InfixToPostfix.cpp
+++ Stack/InfixToPostfix.cpp
@@ -1,160 +1,159 @@
 #include <iostream>
-#include <string>
-#define MAX 20
+#include <cmath>
+#include <stack>
 using namespace std;
 
-// global variables
-char stk[20];
-int top = -1;
+// functions declaration
+bool isOperator(char);
+bool isOperand(char);
+float operation(int, int, char);
+float scanNum(char);
+int precedence(char);
+string postfix(string);
+int postfix_ans(string);
 
-// function declarations
-void push(char oper);
-char pop();
-int priority(char alpha);
-string convert(string infix);
-
-// main functions
+// driver code-----------------
 int main()
 {
-    int cont;
-    string infix, postfix;
-    cout << "\nEnter the infix expression : "; //enter the expression
+    string infix;
+    cout << "Please enter an expression to solve: ";
     cin >> infix;
-    postfix = convert(infix);
+    cout << "The infix expression of the input is : " << infix << endl;
+    string postfix_exp = postfix(infix);
+    cout << "The postfix expression of the input is : " << postfix_exp << endl;
+    cout << "The solution of the expression is : " << postfix_ans(postfix_exp) << endl;
+
     return 0;
 }
+//-----------------------------
 
-// function
-void push(char oper)
+// functions
+bool isOperator(char c)
 {
-    if (top == MAX - 1)
-    {
-        cout << "stackfull!!!!";
-    }
-
+    if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^')
+        return true;
     else
-    {
-        top++;
-        stk[top] = oper;
-    }
+        return false;
 }
-// Function to remove an item from stack.  It decreases top by 1
-char pop()
+
+bool isOperand(char ch)
 {
-    char ch;
-    if (top == -1)
-    {
-        cout << "stackempty!!!!";
-    }
+    if (ch >= '0' && ch <= '9')
+        return true;
     else
-    {
-        ch = stk[top];
-        stk[top] = '\0';
-        top--;
-        return (ch);
-    }
-    return 0;
+        return false;
 }
-int priority(char alpha)
+
+float scanNum(char ch)
 {
-    if (alpha == '+' || alpha == '-')
-    {
-        return (1);
-    }
-
-    if (alpha == '*' || alpha == '/')
-    {
-        return (2);
-    }
-
-    if (alpha == '$')
-    {
-        return (3);
-    }
-
-    return 0;
+    int value;
+    value = ch;
+    return float(value - '0'); //return float from character
 }
-string convert(string infix)
-{
-    int i = 0;
-    string postfix = "";
-    while (infix[i] != '\0')
-    {
-        if (infix[i] >= 'a' && infix[i] <= 'z' || infix[i] >= 'A' && infix[i] <= 'Z')
-        {
-            postfix.insert(postfix.end(), infix[i]);
-            i++;
-        }
-        else if (infix[i] == '(' || infix[i] == '{' || infix[i] == '[')
-        {
-            push(infix[i]);
-            i++;
-        }
-        else if (infix[i] == ')' || infix[i] == '}' || infix[i] == ']')
-        {
-            if (infix[i] == ')')
-            {
-                while (stk[top] != '(')
-                {
-                    postfix.insert(postfix.end(), pop());
-                }
-                pop();
-                i++;
-            }
-            if (infix[i] == ']')
-            {
-                while (stk[top] != '[')
-                {
-                    postfix.insert(postfix.end(), pop());
-                }
-                pop();
-                i++;
-            }
 
-            if (infix[i] == '}')
+int precedence(char c)
+{
+    if (c == '^')
+        return 3;
+    else if (c == '*' || c == '/')
+        return 2;
+    else if (c == '+' || c == '-')
+        return 1;
+    else
+        return -1;
+}
+
+float operation(int a, int b, char op)
+{
+    //Perform operation
+    if (op == '+')
+        return b + a;
+    else if (op == '-')
+        return b - a;
+    else if (op == '*')
+        return b * a;
+    else if (op == '/')
+        return b / a;
+    else if (op == '^')
+        return pow(b, a); //find b^a
+    else
+        return 0; //return negative infinity
+}
+
+string postfix(string infix)
+{
+    stack<char> s;
+    string postfix;
+    for (int i = 0; i < infix.length(); i++)
+    {
+        // if the infix[i] is a number then do this
+        if (isOperand(infix[i]))
+        {
+            postfix = postfix + infix[i];
+        }
+        // if the infix[i] is a opening bracket then do this
+        else if (infix[i] == '(')
+        {
+            s.push(infix[i]);
+        }
+        // if the infix[i] is a closing bracket then do this
+        else if (infix[i] == ')')
+        {
+            while ((s.top() != '(') && (!s.empty()))
             {
-                while (stk[top] != '{')
-                {
-                    postfix.insert(postfix.end(), pop());
-                }
-                pop();
-                i++;
+                char temp = s.top();
+                postfix = postfix + temp;
+                s.pop();
             }
+            if (s.top() == '(')
+            {
+                s.pop();
+            }
+        }
+        else if (isOperator(infix[i]))
+        {
+            if (s.empty() || (precedence(infix[i]) > precedence(s.top())) || ((precedence(infix[i]) == precedence(s.top())) && (infix[i] == '^')))
+            {
+                s.push(infix[i]);
+            }
+            else
+            {
+                while ((!s.empty()) && (precedence(infix[i]) <= precedence(s.top())))
+                {
+                    postfix = postfix + s.top();
+                    s.pop();
+                }
+                s.push(infix[i]);
+            }
+        }
+    }
+    while (!s.empty())
+    {
+        postfix = postfix + s.top();
+        s.pop();
+    }
+    return postfix;
+}
+
+int postfix_ans(string postfix)
+{
+    int a, b;
+    stack<float> s;
+    for (int i = 0; i < postfix.length(); i++)
+    {
+        if (isOperator(postfix[i]))
+        {
+            a = s.top();
+            s.pop();
+            b = s.top();
+            s.pop();
+            s.push(operation(a, b, postfix[i]));
         }
         else
         {
-            if (top == -1)
-            {
-                push(infix[i]);
-                i++;
-            }
-
-            else if (priority(infix[i]) <= priority(stk[top]))
-            {
-                postfix.insert(postfix.end(), pop());
-
-                while (priority(stk[top]) == priority(infix[i]))
-                {
-                    postfix.insert(postfix.end(), pop());
-                    if (top < 0)
-                    {
-                        break;
-                    }
-                }
-                push(infix[i]);
-                i++;
-            }
-            else if (priority(infix[i]) > priority(stk[top]))
-            {
-                push(infix[i]);
-                i++;
-            }
+            s.push(scanNum(postfix[i]));
         }
     }
-    while (top != -1)
-    {
-        postfix.insert(postfix.end(), pop());
-    }
-    cout << "The converted postfix string is : " << postfix; //it will print postfix conversion
-    return postfix;
+
+    return s.top();
 }
